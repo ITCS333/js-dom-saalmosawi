359984794cdd22745d793607670de1236cf452f5
Sayed Ali <sayedali.almosawi588@gmail.com>
2026-03-29 16:53:41 +0300

    done with no changes!

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function changeHeadingText() {
    heading = document.getElementById('main-heading');
   heading.textContent = 'DOM Manipulation Challenge';
 }
-changeHeadingText();
 /**
  * Selects the element with the id 'box-to-modify' and changes its background color to 'lightblue'.
  */
@@ -15,7 +14,6 @@ function changeBoxColor() {
   const box = document.getElementById('box-to-modify');
   box.style.backgroundColor = 'lightblue';
 }
-changeBoxColor();
 /**
  * Creates a new <li> element, sets its text content to 'New Item', and appends it to the <ul> with the id 'item-list'.
  */
@@ -26,23 +24,22 @@ function addNewItem() {
   const itemlist = document.getElementById('item-list');
   itemlist.appendChild(newItem);
 }
-addNewItem();
-
 /**
  * Selects the paragraph with the class 'content-para' and adds the class 'highlight' to it.
  */
 function highlightParagraph() {
   // TODO: Implement this function
+  const para = document.querySelector('.content-para');
+  para.classList.add('highlight');
 }
-
 /**
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
   // TODO: Implement this function
+  const element = document.getElementById('to-be-removed');
+  element.remove();
 }
-
-
 // Do not edit the lines below.
 // These lines are for testing purposes.
 if (typeof module !== 'undefined' && module.exports) {
